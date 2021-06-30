ffcb5e84611967f2a570e37013f693e69195c637
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2021-06-30 14:29:07 +0530

    updated
    removed firebase scripts

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function paymentprocess() {
         "description": "Testing gateway",
         "image": "https://example.com/your_logo",
         // "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-        "handler": function (response){
-            savetoDB(response);
-        },
+        // "handler": function (response){
+            
+        // },
         "prefill": {
             "name": "glarizon",
             "email": "glarizon@example.com",
@@ -25,22 +25,3 @@ function paymentprocess() {
     var propay = new Razorpay(options);
     propay.open();
 };
-
-function savetoDB(response) {
-    console.log(response)
-    firebase.database().ref('payment').set({
-        payment_id: response.razorpay_payment_id
-    });
-}
-
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDWpqDpeh37hz8qBsKqfj7EKhz-hL646n0",
-    authDomain: "payment-testing-3e5a3.firebaseapp.com",
-    projectId: "payment-testing-3e5a3",
-    storageBucket: "payment-testing-3e5a3.appspot.com",
-    messagingSenderId: "863455544681",
-    appId: "1:863455544681:web:264c53b904403ec55969f8",
-    measurementId: "G-R506TFBL4R"
-  };
